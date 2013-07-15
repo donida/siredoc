b845e6e207e6f201e5b30697b79a40295081bbd9
Ronaldo Donida <donida@notenida.(none)>
2013-07-15 02:01:49 -0300

    Estrutura Iniciao do Siredoc

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Siredoc::Application.config.secret_key_base = '12214077ee98b4b738fd93f3862dac15b3ce4c1dc7ecc8ff29b6603ae9cdfeb946645615866fdcc79ea334f76d54d6690bffb9e76a0c4ba85cec73fd01c7fa84'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Siredoc::Application.config.session_store :cookie_store, key: '_siredoc_session'
